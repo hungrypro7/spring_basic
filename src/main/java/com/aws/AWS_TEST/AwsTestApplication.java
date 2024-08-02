9c1dabc431f8cdff9ac9152bf0367739e8e7722f
이찬영 <ichan-yeong@ichan-yeong-ui-MacBookPro.local>
2024-08-02 11:53:32 +0900

    add spring files

---
+++ src/main/java/com/aws/AWS_TEST/AwsTestApplication.java
@@ -0,0 +1,13 @@
+package com.aws.AWS_TEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+class AwsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aws/AWS_TEST/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.AWS_TEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
